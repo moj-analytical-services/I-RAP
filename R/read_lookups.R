6e6cc8a106b7b88053bb856dd7e564452e1a9d55
phillip hall <philip.hall@justice.gsi.gov.uk>
2021-09-28 11:43:20 +0000

    Edited read_lookups to add age group specifications to dataset

--- R/read_lookups.R
+++ R/read_lookups.R
@@ -4,11 +4,17 @@ read_lookups <- function(SHA="main") {
   stop_for_status(req)
   filelist <- unlist(lapply(content(req)$tree, "[", "path"), use.names = F)
   
-  filelist <- filelist[stringr::str_detect(filelist,"reference_files/[a-z]+.csv")]
+  filelist <- filelist[stringr::str_detect(filelist,"reference_files/[a-z_]+.csv")]
   
-  lookups <- lapply(filelist,FUN=function(x){read.csv(paste0("https://raw.github.com/moj-analytical-services/harmonisation-reference/",SHA,"/",x))})
+  lookups <- lapply(filelist[-stringr::str_which(filelist,"age")],FUN=function(x){read.csv(paste0("https://raw.github.com/moj-analytical-services/harmonisation-reference/",SHA,"/",x))})
   
-  names(lookups) <- str_sub(basename(filelist),1,-5)
+  agespec <- lapply(filelist[stringr::str_which(filelist,"age")],FUN=function(x){read.csv(paste0("https://raw.github.com/moj-analytical-services/harmonisation-reference/",SHA,"/",x))})
+  
+  names(agespec) <- str_sub(basename(filelist[stringr::str_which(filelist,"age")]),1,-5)
+  
+  lookups[[length(lookups)+1]] <- agespec
+  
+  names(lookups) <- c(str_sub(basename(filelist[-stringr::str_which(filelist,"age")]),1,-5),"agespec")
   
   return(lookups)
 }
