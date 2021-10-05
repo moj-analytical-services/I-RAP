2d94eebbb9345053d5351887e0236f4e0fd051bc
phillip hall <philip.hall@justice.gsi.gov.uk>
2021-10-05 20:12:57 +0000

    Edited join_lookups function to allow a variable from the lookup to be included in the original data.
    When the lookup is joined, it will replace any values in the original variable with the lookup values.

--- R/join_lookups.R
+++ R/join_lookups.R
@@ -4,13 +4,37 @@ join_lookups <- function(data,lookups,join_vars,SHA="main") {
   
   for (i in 1:length(lookups)) {
     
+    add_vars <- names(lookuplist[[lookups[i]]])[!names(lookuplist[[lookups[i]]]) %in% join_vars]
+    
     if (i == 1) {
     
-      joined <- dplyr::left_join(data,lookuplist[[lookups[i]]],by=join_vars[i])
+      joined <- dplyr::left_join(data,lookuplist[[lookups[i]]],by=join_vars[i],suffix=c(".OLD",".NEW"))
+      
+      for (j in 1:length(add_vars)) {
+      
+        if (add_vars[j] %in% names(data)) {
+          
+          joined <- joined %>% mutate("{add_vars[j]}" := do.call(coalesce,across(c(paste0(add_vars[j],c(".NEW",".OLD"))))),
+                                    .keep = "unused")
+        }
+      
+      }
       
     } else {
       
-      joined <- dplyr::left_join(joined,lookuplist[[lookups[i]]],by=join_vars[i])
+      original <- joined
+      
+      joined <- dplyr::left_join(joined,lookuplist[[lookups[i]]],by=join_vars[i],suffix=c(".OLD",".NEW"))
+      
+      for (j in 1:length(add_vars)) {
+        
+        if (add_vars[j] %in% names(original)) {
+          
+          joined <- joined %>% mutate("{add_vars[j]}" := do.call(coalesce,across(c(paste0(add_vars[j],c(".NEW",".OLD"))))),
+                                    .keep = "unused")
+        }
+        
+      }
       
     }
   }
